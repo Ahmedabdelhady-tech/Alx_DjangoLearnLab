cb94ffac9fadfb36ee67dfb869717aa441d1602e
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-11-17 20:54:15 +0200

    task3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
 
+# --- Models for Library System ---
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -15,12 +19,14 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
+
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
@@ -30,11 +36,10 @@ class Librarian(models.Model):
         return self.name
 
 
-from django.db import models
-from django.contrib.auth.models import User
-
+# --- User Profile and Roles ---
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
     ADMIN = 'Admin'
     LIBRARIAN = 'Librarian'
     MEMBER = 'Member'
@@ -43,19 +48,22 @@ class UserProfile(models.Model):
         (LIBRARIAN, 'Librarian'),
         (MEMBER, 'Member'),
     ]
+    
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=MEMBER)
 
     def __str__(self):
         return f'{self.user.username} - {self.role}'
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
+# --- Signals for Creating and Saving UserProfile ---
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """Automatically create a UserProfile when a User is created."""
     if created:
         UserProfile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    """Automatically save UserProfile when the User is saved."""
     instance.userprofile.save()
