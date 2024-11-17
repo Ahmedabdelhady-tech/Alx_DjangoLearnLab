f023d9cee374eec052c89d392bd9474970178794
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-11-17 20:37:08 +0200

    tasن3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -28,3 +28,37 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    ADMIN = 'Admin'
+    LIBRARIAN = 'Librarian'
+    MEMBER = 'Member'
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (LIBRARIAN, 'Librarian'),
+        (MEMBER, 'Member'),
+    ]
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=MEMBER)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
