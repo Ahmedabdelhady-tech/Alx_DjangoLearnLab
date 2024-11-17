161ee7a0b2c6a7c8dab354f5b5f08642a7c103ec
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-11-17 23:37:07 +0200

    edit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,75 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+# Create your models here.
 
 
-# --- Models for Library System ---
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-# --- User Profile and Roles ---
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    ADMIN = 'Admin'
-    LIBRARIAN = 'Librarian'
-    MEMBER = 'Member'
     ROLE_CHOICES = [
-        (ADMIN, 'Admin'),
-        (LIBRARIAN, 'Librarian'),
-        (MEMBER, 'Member'),
+        ('admin', 'Admin'),
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
     ]
-    
-    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=MEMBER)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
 
     def __str__(self):
-        return f"{self.user.username} ({self.get_role_display()})"
-
-
-# --- Signals for Creating and Saving UserProfile ---
+        return f"{self.user.username} - {self.get_role_display()}"
+    
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Automatically create a UserProfile when a User is created."""
     if created:
         UserProfile.objects.create(user=instance)
 
-
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    """Automatically save UserProfile when the User is saved."""
-    instance.Userprofile.save()
+    instance.profile.save()
 
-
-from django.db import models
-from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -78,12 +33,11 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
 
     class Meta:
         permissions = [
@@ -91,3 +45,22 @@ class Book(models.Model):
             ('can_change_book', 'Can change book'),
             ('can_delete_book', 'Can delete book'),
         ]
+    
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return self.name
