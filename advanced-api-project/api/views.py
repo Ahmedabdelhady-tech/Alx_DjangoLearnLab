bdb5fc61ab00150762d96423dbfd13b72a6db202
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-08 23:29:05 +0200

    task1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
