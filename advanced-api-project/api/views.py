bd20eb51a708489cda6169de2ba7569b67261572
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-09 00:00:33 +0200

    task2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework  
+from rest_framework.filters import OrderingFilter  
 from rest_framework import generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,7 +9,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.filters import OrderingFilter  
 
 # ListView: Retrieve all books with filtering, searching, and ordering
 class BookListView(generics.ListCreateAPIView):
