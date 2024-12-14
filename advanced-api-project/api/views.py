0e1cd70a80fd16638155827f2d6815752d4b4c58
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-15 00:04:47 +0200

    task2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
+from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters import rest_framework as filters
-from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
