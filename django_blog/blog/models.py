14c3a894641a8ebe1965c0544f10ed9a6f931eab
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-15 21:46:16 +0200

    django_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,48 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from taggit.managers import TaggableManager
 
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, related_name='tags')
     tags = TaggableManager()
 
-    def __str__(self):
-        return self.title
-    
-    def get_absolute_url(self):    
-        return reverse('post-detail', kwargs={'pk': self.pk})
-    
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
-    def __str__(self):
-        return f"{self.user.username}'s Profile"
-    
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.width > 100 or img.height > 100:
-            output_size = (100, 100)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
