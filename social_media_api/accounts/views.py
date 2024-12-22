a830d30fda491979a23fc02623a297f4a0d1a06e
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-22 21:57:13 +0200

    User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,9 @@
-from rest_framework import status, views
+from rest_framework import status, views, generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import UserRegistrationSerializer, UserLoginSerializer,comm
+from django.shortcuts import get_object_or_404
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer, PostSerializer
+from .models import CustomUser, Post
 
 # View for user registration
 class UserRegistrationView(views.APIView):
@@ -21,30 +23,27 @@ class UserLoginView(views.APIView):
             return Response({"token": serializer.validated_data['token']}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'followed'}, status=status.HTTP_200_OK)
 
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+
+class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-
-
-
-    
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
