e244d9dc112141179dbf6c7e82326b116817a4d6
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-22 23:11:01 +0200

    task

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,27 +1,7 @@
-from rest_framework import status, views, generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 from django.shortcuts import get_object_or_404
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from .models import CustomUser
-
-# View for user registration
-class UserRegistrationView(views.APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = UserRegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({"message": "User registered successfully", "token": user.auth_token.key}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# View for user login (getting token)
-class UserLoginView(views.APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response({"token": serializer.validated_data['token']}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
