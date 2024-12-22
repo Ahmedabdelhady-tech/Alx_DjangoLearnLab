a967e39585a970eafbb07d143c844e88b166a0a9
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-22 23:20:23 +0200

    task

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,21 +6,3 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     followers = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='following')
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='customuser_set',  
-        blank=True,
-        help_text='The groups this user belongs to.',
-        related_query_name='customuser',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='customuser_permissions', 
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_query_name='customuser',
-    )
-    
-    def __str__(self):
-        return self.username
