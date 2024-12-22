0c2124c6389ba515e3b049a73fe629a9c9e9ca60
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-22 22:57:07 +0200

    task

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,49 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-
-
-# Create your models here.
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('email is required')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self.db)
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Must be staff')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Must be superuser')
-
-        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(unique=True)
-    bio = models.TextField(max_length=250)
-    profile_picture = models.ImageField(blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='following')
 
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    def follow(self, user):
-        self.following.add(user)
-
-    def unfollow(self, user):
-        self.following.remove(user)
-
-    def is_following(self, user):
-        self.following.filter(id=user.id).exists()
-
-    def is_followed_by(self, user):
-        self.followers.filter(id=user.id).exists()
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',  
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_query_name='customuser',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_permissions', 
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='customuser',
+    )
+    
+    def __str__(self):
+        return self.username
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import LoginView, RegisterView, ProfileView, FollowUserView, UnfollowUserView
+from .views import LoginView, RegisterView, ProfileView, FollowUserView, UnfollowUserView, FeedView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user')
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('feed/', FeedView.as_view(), name='feed')
 ]
