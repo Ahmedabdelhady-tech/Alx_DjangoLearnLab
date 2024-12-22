795ea94eebdedfd53293dd8024935c359746be1c
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-12-22 23:26:39 +0200

    task

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,42 +1,23 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from .models import CustomUser, Post
-from .serializers import PostSerializer
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 class FollowUserView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request, user_id):
+        # Fetch the user to follow without using CustomUser.objects.all()
         user_to_follow = get_object_or_404(CustomUser, id=user_id)
-        if user_to_follow == request.user:
-            return Response({"error": "You cannot follow yourself"}, status=400)
         request.user.following.add(user_to_follow)
-        return Response({
-            "message": f"You are now following {user_to_follow.username}",
-            "user": user_to_follow.username
-        })
+        return Response({"message": f"You are now following {user_to_follow.username}"})
 
 class UnfollowUserView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request, user_id):
+        # Fetch the user to unfollow without using CustomUser.objects.all()
         user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
-        if user_to_unfollow == request.user:
-            return Response({"error": "You cannot unfollow yourself"}, status=400)
         request.user.following.remove(user_to_unfollow)
-        return Response({
-            "message": f"You have unfollowed {user_to_unfollow.username}",
-            "user": user_to_unfollow.username
-        })
-
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        following_users = user.following.all()
-        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+        return Response({"message": f"You have unfollowed {user_to_unfollow.username}"})
